865530ba27114634cfa58b20da9ba948cacf7b51
Jon Moeller <jmoeller@gmail.com>
2024-04-14 13:35:34 -0700

    Added function to search for HP WMI device by name

--- hotkey.c
+++ hotkey.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <libudev.h>
 #include <linux/input.h>
 #include <linux/uinput.h>
 #include <stdio.h>
@@ -6,10 +7,98 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-// Replace this with the device you want to get data from (e.g. your keyboard)
-#define INPUT_DEVICE "/dev/input/event7"
-// Replace this with the key you want to use for your tablet mode switch
+
+/*
+    Replace this with the device you want to get data from (e.g. your keyboard)
+    Examples:
+    
+    #define INPUT_DEVICE "/dev/input/event10"
+    #define INPUT_DEVICE "/dev/input/by-id/usb-GENERIC_keyboard-event-kbd"
+*/ 
+#define INPUT_DEVICE first_child_event_device("HP WMI hotkeys")
+
+/*
+    Replace this with the key you want to use for your tablet mode switch.
+    Other codes can be found in [input-event-codes.h] 
+*/ 
 #define NEW_TABLET_MODE_KEY KEY_PROG2
+
+/* Looks for the first input event device that is a child of the device
+   with name "device_name" and returns its device node string. */
+const char* first_child_event_device(const char* device_name) {
+    const char* device_path = NULL;
+    struct udev *udev;
+    struct udev_enumerate *enumerate;
+    struct udev_list_entry *devices, *dev_list_entry;
+    struct udev_device *dev, *parent_dev;
+    int found = 0;
+
+    // Create the udev object
+    udev = udev_new();
+    if (!udev) {
+        fprintf(stderr, "Cannot create udev context.\n");
+        return NULL;
+    }
+
+    // Enumerate devices of the "input" subsystem
+    enumerate = udev_enumerate_new(udev);
+    udev_enumerate_add_match_subsystem(enumerate, "input");
+    udev_enumerate_scan_devices(enumerate);
+    devices = udev_enumerate_get_list_entry(enumerate);
+
+    // Iterate over the enumerated devices to find the target device based on name property
+    udev_list_entry_foreach(dev_list_entry, devices) {
+        const char *syspath, *devnode, *name;
+
+        syspath = udev_list_entry_get_name(dev_list_entry);
+        dev = udev_device_new_from_syspath(udev, syspath);
+
+        // Access the 'name' attribute directly from syspath
+        name = udev_device_get_sysattr_value(dev, "name");
+        if (name && strcmp(name, device_name) == 0) {
+            // We've found our target device. Now let's find the child eventX device.
+            struct udev_enumerate *enumerate_child = udev_enumerate_new(udev);
+            udev_enumerate_add_match_parent(enumerate_child, dev);
+            udev_enumerate_scan_devices(enumerate_child);
+
+            struct udev_list_entry *children, *child_entry;
+            children = udev_enumerate_get_list_entry(enumerate_child);
+
+            udev_list_entry_foreach(child_entry, children) {
+                const char *child_syspath = udev_list_entry_get_name(child_entry);
+                struct udev_device *child_dev = udev_device_new_from_syspath(udev, child_syspath);
+                devnode = udev_device_get_devnode(child_dev);
+
+                if (devnode) {
+                    printf("Found event device: %s\n", devnode);
+                    found = 1;
+                    device_path = strdup(devnode);
+                    udev_device_unref(child_dev);
+                    break;
+
+                }
+
+                udev_device_unref(child_dev);
+            }
+
+            udev_enumerate_unref(enumerate_child);
+            udev_device_unref(dev);
+            break;
+        }
+
+        udev_device_unref(dev);
+    }
+
+    if (!found) {
+        printf("Event device not found.\n");
+    }
+
+    // Cleanup
+    udev_enumerate_unref(enumerate);
+    udev_unref(udev);
+
+    return device_path;
+}
 
 int main(void)
 {
@@ -29,10 +118,10 @@ int main(void)
     // Setup the virtual device
     memset(&uidev, 0, sizeof(uidev));
     snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "Fake Intel HID switches");
-    uidev.id.bustype = BUS_USB;
-    uidev.id.vendor = 0x0;
-    uidev.id.product = 0x0;
-    uidev.id.version = 0x0;
+    uidev.id.bustype = BUS_HOST;
+    uidev.id.vendor = 0x1234;
+    uidev.id.product = 0x1234;
+    uidev.id.version = 0x1234;
 
     // Enable switch events
     ioctl(uinput_fd, UI_SET_EVBIT, EV_SW);
@@ -52,13 +141,14 @@ int main(void)
     event_fd = open(INPUT_DEVICE, O_RDONLY | O_NONBLOCK);
     if (event_fd < 0)
     {
-        perror("Failed to open " INPUT_DEVICE);
+        perror("Failed to open device:");
+        perror(INPUT_DEVICE);
         ioctl(uinput_fd, UI_DEV_DESTROY);
         close(uinput_fd);
         return -1;
     }
 
-    // Main loop to wait for KEY_PROG2 events from INPUT_DEVICE
+    // Main loop to wait for events from INPUT_DEVICE
     // TODO: Add signal handler to stop gracefully
     while (1)
     {
